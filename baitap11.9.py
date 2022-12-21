25052e12015d9c697faa51234b72135326449e1b
LDTung0004 <117810612+Tung0004@users.noreply.github.com>
2022-12-21 15:07:26 +0700

    Add files via upload

---
+++ baitap11.9.py
@@ -0,0 +1,6 @@
+def party_late(arrivals,name):
+    return name in arrivals
+arrivals=['Adela','Fleda','Owen','May','Mora','Gilbert','Ford']
+print(party_late(arrivals,name='Gilbert'))
+print(party_late(arrivals,name='Tony'))
+print(party_late(arrivals,name='Mora'))
